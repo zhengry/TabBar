a6a39a55d90bba2c33a58f5245d3f7791191479d
zhengruyan <ruyanzheng@126.com>
2016-12-26 14:24:18 +0800

    Initial Commit

---
+++ TabBarTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabBarTest
+//
+//  Created by zry on 2016/12/26.
+//  Copyright © 2016年 ZRY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabBarTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabBarTest
+//
+//  Created by zry on 2016/12/26.
+//  Copyright © 2016年 ZRY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
